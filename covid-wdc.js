3d01d1578e7f09a980247e75ecac9afb5e3afa4b
Bishal Pandey <pandeyb@deakin.edu.au>
2020-08-04 01:46:32 +1000

    change getschema func

--- covid-wdc.js
+++ covid-wdc.js
@@ -7,40 +7,95 @@
 
         var source = tableau.connectionData;
 
-        $.ajax({
-            url: source,
-            dataType: "text"
-        }).done(successFunction);
+        // $.ajax({
+        //     url: source,
+        //     dataType: "text"
+        // }).done(successFunction);
 
-        function successFunction(data) {
-            var data = data.replace(/\"/g, "");
-            var data = data.replace(/ /g, '');
-            var allRows = data.split(/\r?\n|\r/);
-            var cols = [];
-            for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
-                var rowCells = allRows[singleRow].split(',');
-                for (var rowCell = 0; rowCell < rowCells.length; rowCell++) {
-                    if (singleRow === 0) {
-                        x = rowCells[rowCell];
+        // function successFunction(data) {
+        //     var data = data.replace(/\"/g, "");
+        //     var data = data.replace(/ /g, '');
+        //     var allRows = data.split(/\r?\n|\r/);
+        //     var cols = [];
+        //     for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
+        //         var rowCells = allRows[singleRow].split(',');
+        //         for (var rowCell = 0; rowCell < rowCells.length; rowCell++) {
+        //             if (singleRow === 0) {
+        //                 x = rowCells[rowCell];
 
-                        y = {
-                            id: x,
-                            alias: x,
-                            dataType: tableau.dataTypeEnum.string
-                        };
-                        cols.push(y);
-                    }
-                }
-            }
-            console.log(cols);
-            var tableInfo = {
-                id: "Covid19CSV",
-                alias: "covid19csv",
-                columns: cols
-            };
+        //                 y = {
+        //                     id: x,
+        //                     alias: x,
+        //                     dataType: tableau.dataTypeEnum.string
+        //                 };
+        //                 cols.push(y);
+        //             }
+        //         }
+        //     }
+        //     console.log(cols);
+        //     var tableInfo = {
+        //         id: "Covid19CSV",
+        //         alias: "covid19csv",
+        //         columns: cols
+        //     };
 
-            schemaCallback([tableInfo]);
+        //     schemaCallback([tableInfo]);
+        // }
+
+        var cols_tableau = [{
+            id: "ID",
+            dataType: tableau.dataTypeEnum.int
+        }, {
+            id: "Updated",
+            dataType: tableau.dataTypeEnum.date
+        }, {
+            id: "Confirmed",
+            dataType: tableau.dataTypeEnum.int
+        }, {
+            id: "ConfirmedChange",
+            dataType: tableau.dataTypeEnum.int
+        }, {
+            id: "Deaths",
+            dataType: tableau.dataTypeEnum.int
+        }, {
+            id: "DeathsChange",
+            dataType: tableau.dataTypeEnum.int
+        }, {
+            id: "Recovered",
+            dataType: tableau.dataTypeEnum.int
+        }, {
+            id: "RecoveredChange",
+            dataType: tableau.dataTypeEnum.int
+        }, {
+            id: "Latitude",
+            dataType: tableau.dataTypeEnum.float
+        }, {
+            id: "Longitude",
+            dataType: tableau.dataTypeEnum.float
+        }, {
+            id: "ISO2",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "ISO3",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "Country_Region",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "AdminRegion1",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "AdminRegion2",
+            dataType: tableau.dataTypeEnum.string
+        }];
+
+        var tableSchema = {
+            id: "Covid19CSV",
+            alias: "covid19csv",
+            columns: cols_tableau
         }
+
+        schemaCallback([tableSchema]);
 
 
     };
@@ -67,7 +122,7 @@
                         y = {
                             id: x,
                             alias: x,
-                            dataType: tableau.dataTypeEnum.string
+                            //dataType: tableau.dataTypeEnum.string
                         };
                         cols.push(y);
                     }
