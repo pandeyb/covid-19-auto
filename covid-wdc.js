f2128fa0d53ac08500001f59fa4c3ff4ea535575
Bishal Pandey <pandeyb@deakin.edu.au>
2020-08-04 00:09:22 +1000

    initial commit

---
+++ covid-wdc.js
@@ -0,0 +1,102 @@
+(function () {
+
+    var myConnector = tableau.makeConnector();
+
+
+    myConnector.getSchema = function (schemaCallback) {
+
+        var source = tableau.connectionData;
+
+        $.ajax({
+            url: source,
+            dataType: "text"
+        }).done(successFunction);
+
+        function successFunction(data) {
+            var data = data.replace(/\"/g, "");
+            var data = data.replace(/ /g, '');
+            var allRows = data.split(/\r?\n|\r/);
+            var cols = [];
+            for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
+                var rowCells = allRows[singleRow].split(',');
+                for (var rowCell = 0; rowCell < rowCells.length; rowCell++) {
+                    if (singleRow === 0) {
+                        x = rowCells[rowCell];
+
+                        y = {
+                            id: x,
+                            alias: x,
+                            dataType: tableau.dataTypeEnum.string
+                        };
+                        cols.push(y);
+                    }
+                }
+            }
+            console.log(cols);
+            var tableInfo = {
+                id: "Covid-19-CSV",
+                alias: "covid-19-csv",
+                columns: cols
+            };
+
+            schemaCallback([tableInfo]);
+        }
+
+
+    };
+
+    myConnector.getData = function (table, doneCallback) {
+
+        var source = tableau.connectionData;
+
+        $.ajax({
+            url: source,
+            dataType: "text",
+        }).done(successFunction);
+
+        function successFunction(data) {
+            var data = data.replace(/\"/g, "");
+            var allRows = data.split(/\r?\n|\r/);
+            var tableData = [];
+            var cols = [];
+            for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
+                var rowCells = allRows[singleRow].split(',');
+                for (var rowCell = 0; rowCell < rowCells.length; rowCell++) {
+                    if (singleRow === 0) {
+                        x = rowCells[rowCell];
+                        y = {
+                            id: x,
+                            alias: x,
+                            dataType: tableau.dataTypeEnum.string
+                        };
+                        cols.push(y);
+                    }
+                }
+                if (singleRow != 0) {
+                    x = rowCells;
+
+                    tableData.push(x);
+                }
+            }
+
+            table.appendRows(tableData);
+            doneCallback();
+        }
+    };
+
+    tableau.registerConnector(myConnector);
+})();
+
+
+
+
+$(document).ready(function () {
+
+    $("#submitButton").click(function () {
+        datasource = "https://raw.githubusercontent.com/microsoft/Bing-COVID-19-Data/master/data/Bing-COVID19-Data.csv";
+        tableau.connectionData = datasource;
+        tableau.connectionName = "Covid-19 Data";
+        tableau.submit();
+
+    });
+});
