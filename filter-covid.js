f7c72d1b172f95d5baee5a005ce7301a633ab71b
Bishal Pandey <pandeyb@deakin.edu.au>
2020-08-13 02:15:42 +1000

    json test

--- filter-covid.js
+++ filter-covid.js
@@ -95,8 +95,8 @@ d3.csv("https://raw.githubusercontent.com/microsoft/Bing-COVID-19-Data/master/da
     //var tbody = document.createElement("tbody")
     //mainTable.appendChild(tbody)
     //var tbody = document.querySelector("#dataTable");
-    console.log(JSON.stringify(d))
-    document.getElementById("content").appendChild(buildTable(d));
+    JSON.stringify(d)
+    //document.getElementById("content").appendChild(buildTable(d));
     //addDataToTbody(tbody, d);
     // //console.log(d.length, d);
     // var csvContent = convertArrayOfObjectsToCSV({
